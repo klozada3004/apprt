616f1d11d2d420dc49b654b95b00f410f4f0aa9e
oscar arias <>
2024-08-28 16:04:06 -0500

    creamos base para proyecto

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const router =require('express').Router();
 
-router.get('/', (req, res)=> {
-    res.send('Index');
-})
+router.get('/login', (req, res)=> {
+    res.render('../views/login');
+});
+router.get('/register', (req, res)=> {
+    res.render('../views/register');
+});
 
 module.exports = router;
